b02309dbcb3bb66a61878b755e7a35e944fd1811
Ryan <90571282+issryan@users.noreply.github.com>
2024-12-17 19:58:07 -0500

    updated icons on menu

--- src/games.js
+++ src/games.js
@@ -1,36 +1,36 @@
 export const games = [
     {
-        logo: '',
+        logo: '/games/about.png',
         name: 'About',
         description: 'Learn More About Me',
         bg: ''
     },
     {
-        logo: '/games/galleryIcon.png',
+        logo: '/games/gallery.png',
         name: 'Gallery',
         description: 'See some of my favorite pictures',
         bg: ''
     },
     {
-        logo: '',
+        logo: '/games/binge.png',
         name: 'BingeBuddy',
         description: 'TV Show Tracker & Rater',
         bg: ''
     },
     {
-        logo: '',
+        logo: '/games/breed.png',
         name: 'Breed Finder',
         description: 'An AI-powered app to identify dog breeds using machine learning.',
         bg: ''
     },
     {
-        logo: '',
+        logo: '/games/flick.png',
         name: 'Flick',
         description: 'Mini Social Media App',
         bg: ''
     },
     {
-        logo: '',
+        logo: '/games/shift.png',
         name: 'Shift Happens',
         description: "In Development",
         bg: ''
